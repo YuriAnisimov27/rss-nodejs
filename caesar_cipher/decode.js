ef70a8ed5f8aca02038860b500378a94202700bd
Yuri Anisimov <yurianisimov27@gmail.com>
2021-05-04 20:56:57 +0300

    feat: implement decode function

---
+++ caesar_cipher/decode.js
@@ -0,0 +1,26 @@
+const alphabet = require('./alphabet.json');
+
+/**
+ * Function decode a text by Caesar cipher
+ * @param {String} str initial text
+ * @param {Number} shift parameter indicates how many elements to shift
+ * @returns {String} returns decoded text
+ */
+function decode(str, shift) {
+  let res = '';
+  for (let char of str) {
+    const uppChar = char.toUpperCase();
+    if (alphabet.includes(uppChar)) {
+      const idx = alphabet.findIndex(el => el === uppChar);
+      const ciphered = alphabet[(idx + shift) % alphabet.length];
+      const cipheredChar = uppChar === char ? ciphered : ciphered.toLowerCase();
+      res += cipheredChar;
+    } else {
+      res += char;
+    }
+  }
+
+  return res;
+}
+
+module.exports = decode;
