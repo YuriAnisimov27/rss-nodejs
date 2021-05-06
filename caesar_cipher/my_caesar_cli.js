c189b7930647789607c22c682c586fe3f0b50768
Yuri Anisimov <yurianisimov27@gmail.com>
2021-05-06 18:49:40 +0300

    refactor: removed command line parsing in separate module

--- caesar_cipher/my_caesar_cli.js
+++ caesar_cipher/my_caesar_cli.js
@@ -1,38 +1,4 @@
-const fs = require('fs');
-const path = require('path');
-const { program } = require('commander');
-const encode = require('./utils/encode');
-const decode = require('./utils/decode');
-const alphabet = require('./data/alphabet.json');
-
-program.version('1.0.0', '-v, --version', 'output the current version');
-program
-  .requiredOption('-s, --shift <type>', 'The shift is required')
-  .option('-i, --input <type>', 'input file')
-  .option('-o, --output <type>', 'output file')
-  .requiredOption('-a, --action <type>', 'Action (encode/decode) is required');
-program.parse(process.argv);
-
-const options = program.opts();
-
-let input, output;
-
-if (options.input) {
-  const pathToInputFile = path.join(__dirname, options.input);
-  input = fs.createReadStream(pathToInputFile);
-} else {
-  input = process.stdin;
-}
-
-if (options.output) {
-  const pathToOutputFile = path.join(__dirname, options.output);
-  output = fs.createWriteStream(pathToOutputFile, { flags: 'a' });
-} else {
-  output = process.stdout;
-}
-
-const method = options.action === 'encode' ? encode : decode;
-const shift = Number(options.shift) % alphabet.length;
+const { method, shift, input, output } = require('./parse_cl');
 
 input.on('data', function (chunk) {
   output.write(method(chunk.toString(), shift));
---
+++ caesar_cipher/parse_cl.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const fs = require('fs');
+const { program } = require('commander');
+const encode = require('./utils/encode');
+const decode = require('./utils/decode');
+const alphabet = require('./data/alphabet.json');
+
+function parseCommandLine() {
+  program.version('1.0.0', '-v, --version', 'output the current version');
+  program
+    .requiredOption('-s, --shift <type>', 'The shift is required')
+    .option('-i, --input <type>', 'input file')
+    .option('-o, --output <type>', 'output file')
+    .requiredOption(
+      '-a, --action <type>',
+      'Action (encode/decode) is required'
+    );
+  program.parse(process.argv);
+
+  const options = program.opts();
+
+  let input, output;
+
+  if (options.input) {
+    const pathToInputFile = path.join(__dirname, options.input);
+    input = fs.createReadStream(pathToInputFile);
+  } else {
+    input = process.stdin;
+  }
+
+  if (options.output) {
+    const pathToOutputFile = path.join(__dirname, options.output);
+    output = fs.createWriteStream(pathToOutputFile, { flags: 'a' });
+  } else {
+    output = process.stdout;
+  }
+
+  const method = options.action === 'encode' ? encode : decode;
+  const shift = Number(options.shift) % alphabet.length;
+
+  return {
+    method,
+    shift,
+    input,
+    output,
+  };
+}
+
+module.exports = parseCommandLine();
